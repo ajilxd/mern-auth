77531f4482a67072a4f9afd2ef7e59116082084d
ajilxd <149603342+ajilxd@users.noreply.github.com>
2024-06-30 14:37:25 +0530

    created sign in api

--- api/controller/authController.js
+++ api/controller/authController.js
@@ -1,5 +1,7 @@
 import User from "../models/userModel.js";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+import bcryptjs from "bcrypt";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -14,4 +16,23 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  console.log(req.body);
+  const validUser = await User.findOne({ email });
+  if (!validUser) return next(errorHandler(404, "user not found"));
+  console.log(validUser);
+  const validPassword = bcryptjs.compareSync(password, validUser.password);
+  if (!validPassword) return next(errorHandler(401, "wrong credentials"));
+  const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+  const user = validUser.toObject();
+  delete user.password; // Explicitly delete the password field
+  const maxAge = 24 * 60 * 60 * 1000; // 1 day in milliseconds
+
+  res
+    .cookie("access_token", token, { httpOnly: true, maxAge: maxAge })
+    .status(200)
+    .json(user);
 };
